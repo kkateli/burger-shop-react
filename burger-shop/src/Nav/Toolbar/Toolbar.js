41b2286b260a966bf3bfecbaf0cf132565329fbe
kate li <kateli24@outlook.com>
2019-05-07 15:24:46 +1200

    Added logo

--- burger-shop/src/Nav/Toolbar/Toolbar.js
+++ burger-shop/src/Nav/Toolbar/Toolbar.js
@@ -1,12 +1,17 @@
 //this is toolbar component(nav bar)
 import React from "react";
 import cssClasses from "./Toolbar.module.css";
+import logo from "../../assets/images/burger-logo.png";
+
 const toolbar = () => {
   return (
-    <div className = {cssClasses.Toolbar}>
-      
-      {/* <img src= alt="logo" /> */}
-      <p><i class="fas fa-align-justify"></i></p>
+    <div className={cssClasses.Toolbar}>
+      <p>
+        <i class="fas fa-align-justify" />
+      </p>
+      <div className={cssClasses.Logo}>
+        <img src={logo} alt="logo" />
+      </div>
     </div>
   );
 };
