679ee13f8a8e3a6e535db478fff839cc0b2f20da
kate li <kateli24@outlook.com>
2019-06-03 00:14:10 +1200

    Dispatched email and password, posted them to Firebase

--- burger-shop/src/actions/auth.js
+++ burger-shop/src/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const authStart = () => {
   return {
     type: "AUTH-START"
@@ -21,5 +23,21 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData={
+        email:email,
+        password:password,
+        returnSesureToken:true
+    }
+    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB_ovwqdXVswNx3oRc3UXEYQm-6EZxvmRg',authData)
+    .then(response=>{
+        console.log(response.data);
+        dispatch(authSuccess(response.data));
+
+    })
+    .catch(err=>{
+        console.log(err);
+        dispatch(authFail(err));
+    })
+
   };
 };
