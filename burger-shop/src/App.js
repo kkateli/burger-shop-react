b28ad04154911f47c382b17aab444597fd8d0045
kate li <kateli24@outlook.com>
2019-06-05 21:03:43 +1200

    Provented unauth user to access protected pages by modifying url

--- burger-shop/src/App.js
+++ burger-shop/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component}from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Layout from "../src/Components/Layout/Layout";
@@ -9,36 +9,53 @@ import S from "./Components/Secret/Secret";
 import Logout from "./Components/Logout/Logout";
 import { connect } from "react-redux";
 import { authCheck } from "./actions/auth";
+import {Redirect} from "react-router-dom";
 
-class App extends Component{
-  componentDidMount(){
+class App extends Component {
+  componentDidMount() {
     return this.props.checkAuth();
   }
-  render(){
+  render() {
+    let routes = (
+      <Switch>
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/auth" component={Auth} />
+        <Redirect to="/"/>
+      </Switch>
+    );
+
+    if (this.props.ifAuth) {
+      routes = (
+        <Switch>
+          <Route path="/" exact component={BurgerBuilder} />
+          
+          <Route path="/check-out" component={CheckOut} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/secret" component={S} />
+          <Redirect to="/"/>
+        </Switch>
+      );
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/" exact component={BurgerBuilder} />
-            <Route path="/check-out" component={CheckOut} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/secret" component={S} />
-          </Switch>
+          {routes}
         </Layout>
+       
       </div>
     );
-
   }
-  
-  
 }
+
+const mapStateToProps = state => {
+  return { ifAuth: state.auth.token != null };
+};
 
 const mapDispatchToProps = dispatch => {
   return { checkAuth: () => dispatch(authCheck()) };
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
